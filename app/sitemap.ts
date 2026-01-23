0de05fe03473c117b94c08e362c80bf7bc809590
George Zavlanis <90002254+gzavlanis@users.noreply.github.com>
2026-01-23 14:23:55 +0200

    fix the urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,22 +4,16 @@ import { BASE_URL } from "@/app/lib/constants";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: BASE_URL,
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
-        },
-        {
             url: `${BASE_URL}/en`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.8,
+            priority: 1,
         },
         {
             url: `${BASE_URL}/el`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.8,
+            priority: 1,
         },
     ];
 }
