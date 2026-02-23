b24861f7e964235c831365ba0816af91de77dfe6
George Zavlanis <90002254+gzavlanis@users.noreply.github.com>
2026-02-24 00:30:58 +0200

    correction for middleware file

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 const locales = ['en', 'el'];
 const defaultLocale = 'en';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
 
     if (pathname === '/') {
@@ -20,6 +20,8 @@ export function middleware(request: NextRequest) {
             new URL(`/${defaultLocale}${pathname}`, request.url)
         );
     }
+
+    return NextResponse.next();
 }
 
 export const config = {
