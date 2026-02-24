07fa5f63ebd5e7a41036137598a17d37f626480c
George Zavlanis <90002254+gzavlanis@users.noreply.github.com>
2026-02-24 20:16:16 +0200

    Final corrections

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -3,7 +3,7 @@ import {ResumeData} from "@/app/lib/data";
 export default function Experience({ data }: { data: ResumeData['experience'] }) {
     return (
         <section>
-            <h2 className="font-playfair text-3xl font-bold mb-8 text-neutral-900 dark:text-white border-l-4 border-neutral-900 dark:border-white pl-4">
+            <h2 className="font-playfair text-3xl font-bold mb-8 text-neutral-900 dark:text-neutral-300 border-l-4 border-neutral-900 dark:border-white pl-4">
                 {data.title}
             </h2>
 
@@ -11,7 +11,7 @@ export default function Experience({ data }: { data: ResumeData['experience'] })
                 {data.jobs.map((job, index) => (
                     <div key={index} className="group">
                         <div className="flex flex-col md:flex-row md:justify-between md:items-baseline mb-2">
-                            <h3 className="text-xl font-bold text-neutral-800 dark:text-white">{job.role}</h3>
+                            <h3 className="text-xl font-bold text-neutral-600 dark:text-neutral-300">{job.role}</h3>
                             <span className="font-serif italic text-neutral-600 dark:text-neutral-300">{job.period}</span>
                         </div>
 
@@ -19,7 +19,7 @@ export default function Experience({ data }: { data: ResumeData['experience'] })
                             {job.company} • {job.location}
                         </div>
 
-                        <ul className="list-disc list-outside ml-4 space-y-2 text-neutral-600 dark:text-neutral-400 leading-relaxed font-light text-justify">
+                        <ul className="list-disc list-outside ml-4 space-y-2 text-neutral-600 dark:text-neutral-300 leading-relaxed font-light text-justify">
                             {job.description.map((desc, i) => (
                                 <li key={i}>{desc}</li>
                             ))}
